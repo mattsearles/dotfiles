207de2652b15c4f19818c504e00c79400b25616f
Matt Searles <matt@searles.com>
2020-02-25 11:56:33 +0000

    Removing invalid install options

--- brew.sh
+++ brew.sh
@@ -37,7 +37,7 @@ fi;
 brew install wget
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim
 brew install grep
 brew install openssh
 brew install screen
